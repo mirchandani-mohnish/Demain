35610815a66edda5f3739f54d3609ecffbdb18b0
Mohnish Mirchandani <mirchandani.mohnish03@gmail.com>
2022-04-06 12:30:40 +0530

    Update: Minor additions

--- frontend/src/components/Suggestions.jsx
+++ frontend/src/components/Suggestions.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { Fade } from "react-reveal";
 
 const Suggestions = () => {
-  return <div></div>;
+  return (
+    <div>
+      <div>
+        <div className="container flex justify-between justify-items-center align-middle h-screen">
+          <Fade left>
+            <div className="flex-item justify-center align-middle m-auto w-1/2 md:w-full sm:w-full">
+              <img src="/feedbackImg.png" alt="pizza-img" />
+            </div>
+          </Fade>
+          <Fade right></Fade>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Suggestions;
